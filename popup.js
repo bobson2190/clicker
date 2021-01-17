28588b364c38046b6cf24a58369fb8fefc3fcc44
Bobka K <74317964+bobson2190@users.noreply.github.com>
2021-01-17 12:55:29 +0100

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-var clickcount = 0;
-var countlabel = document.querySelector("#count");
-var btn = document.querySelector("#buttontorick");
-btn.onclick = function() {
-  clickcount =+ 1;
-  countlabel.innerHTML = clickcount;
-  console.log("Hi!")
-};
